5ae5418bc399d348e3446c277b522c11bb9ad130
KevinBAustin <kevbaust@ut.utm.edu>
2023-10-31 16:03:14 -0500

    Implemented use of driver in main

--- main.cpp
+++ main.cpp
@@ -7,6 +7,65 @@ using namespace std;
 void driver(int &choice);
 
 int main() {
+  int choice=0;
+  library lib;
+  string in_library;
+  string out_library;
+  
+  while(choice!=8){
+    driver(choice);
+    if(choice==1){
+      cout<<"Enter file to be read: ";
+      cin>>in_library;
+      lib.read_from_file(in_library);
+    }
+    if(choice==2){
+      cout<<"Enter file to write library to: ";
+      cin>>out_library;
+      lib.write_to_file(out_library);
+    }
+    if(choice==3){
+      cout<<"Printing library";
+      lib.print();
+    }
+    if(choice==4){
+      string AuthorName;
+      cout<<"Enter Author's name";
+      cin>>AuthorName;
+      cout<<"Searching"<<endl;
+      string AuthorWork=lib.find_author(AuthorName);
+      cout<<AuthorName<<" has written: "<<AuthorWork<<endl;
+    }
+    if(choice==5){
+      string AuthorName;
+      cout<<"Enter Author's name";
+      cin>>AuthorName;
+      cout<<"Searching"<<endl;
+      string AuthorWork=lib.find_album(AuthorName);
+      cout<<AuthorName<<"'s books and information"<<endl;
+      cout<< AuthorWork<<endl;
+    }
+    if(choice==6){
+      string first, last, number;
+      cout<<"Enter first name";
+      cin>>first;
+      cout<<"Enter last name";
+      cin>>last;
+      cout<<"Enter number";
+      cin>>number;
+      phone.insert_sorted(first, last, number);
+      cout<<"User added"<<endl;
+    }
+    if(choice==7){
+      string AuthorName;
+      cout<<"Enter name of to be deleted Author";
+      cin>>AuthorName;
+      lib.Delete(AuthorName);
+    }
+    if(choice==8){
+      cout<<"Alright exiting. Have a Great Life:)"<<endl;
+    }
+  }
   
   return 0;
 }
